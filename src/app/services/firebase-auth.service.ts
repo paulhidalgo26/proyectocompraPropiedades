df15dcfa546d9beb8001caa28f8a826cb64aa2f5
paulhidalgo26 <paulsb26@gmail.com6>
2022-01-26 00:28:51 -0500

    three commit

---
+++ src/app/services/firebase-auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseAuthService {
+
+  constructor(public auth: AngularFireAuth) { }
+
+  login(email: string, password: string) {
+    return this.auth.signInWithEmailAndPassword(email,password);
+  }
+  logout() {
+    this.auth.signOut();
+  }
+  registrar(email: string, password: string){
+   return this.auth.createUserWithEmailAndPassword(email,password);
+  }
+
+}
